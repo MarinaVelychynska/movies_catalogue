43120173ef2069975676dfaab41a7babe77d37eb
Maryna Velychynska <marien5@ukr.net>
2023-03-12 21:53:44 +0100

    tmdb_client.py

--- tmdb_client.py
+++ tmdb_client.py
@@ -46,3 +46,11 @@ def get_movies_list(list_type):
     response = requests.get(endpoint, headers=headers)
     response.raise_for_status()
     return response.json()
+
+def get_single_movie_images(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/images"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()
